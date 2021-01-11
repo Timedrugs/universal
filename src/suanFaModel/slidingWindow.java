4f0e458251511c2723c832c6a041a461913e8272
xiaokuo001 <xiaokuo001@ke.com>
2021-01-11 17:51:38 +0800

    说明

--- src/suanFaModel/slidingWindow.java
+++ src/suanFaModel/slidingWindow.java
@@ -6,6 +6,8 @@ import java.util.HashMap;
 /**
  * @author xiaokuo
  * @since 2021/1/8 12:31 上午
+ *
+ * url: https://mp.weixin.qq.com/s/ioKXTMZufDECBUwRRp3zaA
  */
 public class slidingWindow {
 
@@ -61,6 +63,8 @@ public class slidingWindow {
      *
      * 给你一个字符串 s 、一个字符串 t 。返回 s 中涵盖 t 所有字符的最小子串。
      * 如果 s 中不存在涵盖 t 所有字符的子串，则返回空字符串 "" 。
+     *
+     *
      */
     public String minWindow(String s, String t) {
 
