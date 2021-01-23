3cd8c7368153ac83ab29c4a576a0e4711f11dd42
xiaokuo001 <xiaokuo001@ke.com>
2021-01-23 22:23:42 +0800

    二叉树完结

--- src/suanFaModel/TreeCode/BinaryTreeCode.java
+++ src/suanFaModel/TreeCode/BinaryTreeCode.java
@@ -6,10 +6,11 @@ package suanFaModel.TreeCode;
 
 import apple.laf.JRSUIUtils.Tree;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
- * TODO Description
+ * 二叉树
  *
  * @author xiaokuo
  * info : https://mp.weixin.qq.com/s?__biz=MzAxODQxMDM0Mw==&mid=2247487126&idx=1&sn=4de13e66397bc35970963c5a1330ce18&chksm=9bd7f09eaca0798853c41fba05ad5fa958b31054eba18b69c785ae92f4bd8e4cc7a2179d7838&scene=21#wechat_redirect
@@ -331,6 +332,38 @@ public class BinaryTreeCode {
         return root;
     }
 
+
+    /**
+     * 652 题「寻找重复子树」：
+     * @param root
+     * https://mp.weixin.qq.com/s?__biz=MzAxODQxMDM0Mw==&mid=2247487527&idx=1&sn=9cf2b0d8608ba26ea7c6a5c9b41d05a1&chksm=9bd7ee2faca0673916bf075539bf6fc3c01f3dcc0b298b3f507047692ef5c850ed9cfe82e4e6&scene=178&cur_album_id=1318896187793260544#rd
+     */
+    HashMap<String, Integer> subStrings = new HashMap<>();
+    List<TreeNode> res = new ArrayList<>();
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+
+        traverse652(root);
+        return res;
+    }
+    public String traverse652(TreeNode root){
+        if (root == null){
+            return "#";
+        }
+
+        String  left = traverse652(root.left);
+        String right = traverse652(root.right);
+
+        String subString = left + ":"+ right + ":" + root.val;
+
+        int subNums = subStrings.getOrDefault(subString, 0);
+
+        if (subNums == 1){
+            res.add(root);
+        }
+
+        subStrings.put(subString, ++subNums);
+        return subString;
+    }
 
 
 
