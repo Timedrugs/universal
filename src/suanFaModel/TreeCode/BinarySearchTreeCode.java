1782e24b618d3d0a1dd668042ec021820e01360c
xiaokuo001 <xiaokuo001@ke.com>
2021-02-21 16:09:23 +0800

    二叉搜索树

---
+++ src/suanFaModel/TreeCode/BinarySearchTreeCode.java
@@ -0,0 +1,132 @@
+package suanFaModel.TreeCode;
+
+/**
+ * 二叉搜索树
+ *
+ * @author xiaokuo
+ * @since 2021/1/23 10:19 下午
+ */
+public class BinarySearchTreeCode {
+
+    //第一期：https://mp.weixin.qq.com/s?__biz=MzAxODQxMDM0Mw==&mid=2247488101&idx=1&sn=6041ddda5f20ccde8a7036d3e3a1482c&chksm=9bd7ec6daca0657b2ab20a936437e2c8206384c3b1485fe91747ad796fa3a5b08556b2f4911e&scene=178&cur_album_id=1318892385270808576#rd
+
+    /**
+     * 从做算法题的角度来看 BST，除了它的定义，还有一个重要的性质：BST 的中序遍历结果是有序的（升序）。
+     * <p>
+     * 也就是说，如果输入一棵 BST，以下代码可以将 BST 中每个节点的值升序打印出来：
+     */
+    void traverse(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        traverse(root.left);
+        // 中序遍历代码位置
+        System.out.println(root.val);
+//        print(root.val);
+        traverse(root.right);
+    }
+
+    public static void main(String[] args) {
+        BinarySearchTreeCode cl = new BinarySearchTreeCode();
+//        TreeNode root = new TreeNode(2147483647);
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(1);
+        System.out.println(cl.isValidBST(root));
+    }
+
+
+    /**
+     *  230 题「二叉搜索树中第K小的元素
+     */
+//    public int kthSmallest(TreeNode root, int k) {
+//
+//    }
+
+    /**
+     * 98 验证二叉搜索树
+     * 时间复杂度O(n)
+     * 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+     * 假设一个二叉搜索树具有如下特征：
+     * 节点的左子树只包含小于当前节点的数。
+     * 节点的右子树只包含大于当前节点的数。
+     * 所有左子树和右子树自身必须也是二叉搜索树。
+     */
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean isValidBST(TreeNode node, long min, long max) {
+        if (node == null) {
+            return true;
+        }
+        if (node.val <= min || node.val >= max) {
+            return false;
+        }
+        return isValidBST(node.left, min, node.val) && isValidBST(node.right, node.val, max);
+    }
+
+    /**
+     * 236
+     * 二叉树的最近公共祖先
+     * O(n)
+     *
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        if (root == null || root == p || root == q){
+            return root;
+        }
+
+        TreeNode left =  lowestCommonAncestor(root.left, p, q);
+        TreeNode right =  lowestCommonAncestor(root.right, p, q);
+
+        if (left == null){
+            root = right;
+        }
+
+        if (right == null){
+            root = left;
+        }
+
+        return root;
+    }
+
+    /**
+     * 235
+     * 二叉搜索树的最近公共祖先
+     */
+    public TreeNode lowestCommonAncestor235(TreeNode root, TreeNode p, TreeNode q) {
+
+        if (root == null || root == q || root == p){
+            return root;
+        }
+
+        // 左最大值 < root  右最小值 > root
+        if (root.val < q.val && root.val < p.val){
+            return  lowestCommonAncestor235(root.right, p, q);
+        }
+
+        if (root.val > q.val && root.val > p.val){
+            return lowestCommonAncestor235(root.left, p, q);
+        }
+
+        return root;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
