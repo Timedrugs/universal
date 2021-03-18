bdd8d853575c1e456427c8164b43b67eaf7335da
xiaokuo001 <xiaokuo001@ke.com>
2021-03-18 19:57:46 +0800

    提交多天最哦也

---
+++ src/TrainingCamp/Daily/EightDay.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2021. 贝壳找房（北京）科技有限公司
+ */
+package TrainingCamp.Daily;
+
+import java.util.HashMap;
+
+/**
+ * TODO Description
+ *
+ * @author xiaokuo
+ * @since 2021/3/16 1:31 下午
+ */
+public class EightDay {
+
+    /**
+     *Offer 59 - I. 滑动窗口的最大值
+     * 2中写法
+     * 1. 单调栈  多一些出站 进站判断
+     * 2. 非单调栈
+     */
+//    public int[] maxSlidingWindow(int[] nums, int k) {
+//        int  len = nums.length;
+//        if (len == 0){
+//            return new int[]{};
+//        }
+//
+//        HashMap<Integer, Integer> windows = new HashMap<>();
+//        HashMap<Integer, Integer> needs   = new HashMap<>();
+//
+//        for (int i = 0; i < len; i++){
+//
+//
+//
+//        }
+//    }
+
+}
